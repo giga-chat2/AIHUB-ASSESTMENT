74a25ce10dab4755a328fd45d52939f3c1b61fe8
giga-chat2 <167806079+giga-chat2@users.noreply.github.com>
2024-07-24 00:42:15 +0530

    Update Testimonials.tsx

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -24,14 +24,14 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({ imagePath, name, titl
     <>
       <div className='w-[80%] h-[48vh] rounded-xl my-[5%] shadow-all-around p-[1%] md:w-[40%] md:h-[35%] lg:w-[20%] lg:my-[0] lg:mx-[2%] lg:h-[90%]'>
         {/* User image section */}
-        <div className='w-[100%] h-[50%] flex justify-center items-center'>
+        <div className='w-[100%] h-[40%] flex justify-center items-center'>
           <div className='w-[120px] h-[120px] rounded-full overflow-hidden'>
             <img src={`${imagePath}`} alt='person' className='w-full h-full object-cover' />
           </div>
         </div>
 
         {/* User details section */}
-        <div className='w-[100%] h-[50%] flex flex-col justify-center items-center'>
+        <div className='w-[100%] h-[60%] flex flex-col justify-center items-center'>
           <div className='w-[100%] h-[30%] flex justify-center flex-col items-center overflow-hidden'>
             <p className='text-[#33333b] font-600 text-lg lg:text-3xl whitespace-normal'>{name}</p>
             <p className='text-[#33333b] font-400 text-sm lg:text-xl whitespace-normal'>{title}</p>
